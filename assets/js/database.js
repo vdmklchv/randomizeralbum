607396b2abf5b3e436bebd2a2334d15b91dd0976
Vadim Colcev <vadimkolchev@gmail.com>
2021-03-27 23:50:34 +0200

    Added number of albums output

--- assets/js/database.js
+++ assets/js/database.js
@@ -3,17 +3,18 @@ const app = express();
 app.engine("html", require('ejs').renderFile);
 app.set('view engine', 'html');
 const { MongoClient } = require('mongodb');
-const albums = [];
+let albums = [];
 
 let artist;
 let album;
+let albumNumber;
 
 app.use(express.static('public'));
 
 
 app.get("/", (req, res) => {
     main().then(() => {
-        res.render("index.html", { artist: artist, album: album });
+        res.render("index.html", { artist: artist, album: album, albumNumber: albumNumber });
     })
 })
 
@@ -24,6 +25,7 @@ app.get("/next-album", (req, res) => {
 
 
 async function main() {
+    albums = [];
     const uri = "mongodb+srv://vdmclcv:albumdbpass@cluster0.t1wlx.mongodb.net/albums?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
@@ -36,6 +38,7 @@ async function main() {
         //console.log(`Next you should listen to ${randomAlbum.name} by ${randomAlbum.artist}`);
         artist = randomAlbum.artist;
         album = randomAlbum.name;
+        albumNumber = albums.length;
     });
 }
 
