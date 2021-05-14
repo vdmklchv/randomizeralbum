0a8abfb08fa6a4e749bc8899e4b994bc34e7c78d
Vadim Colcev <vadimkolchev@gmail.com>
2021-05-15 00:41:42 +0300

    Added cors to preflight

--- assets/js/database.js
+++ assets/js/database.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.options('*', cors());
 // connecting to db
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
